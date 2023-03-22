12d72000a5acc0635c7bae6012698eff7b0a0cb1
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2023-03-22 09:32:28 +0100

    added view, registered in admin.py

--- books/views.py
+++ books/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Book
+from .serializers import BooksSerializer
+from rest_framework.response import Response
+
 
 # Create your views here.
+class BookList(APIView):
+    def get(self, request):
+        queryset = Book.objects.all()
+        serializer = BooksSerializer
+        return Response(serializer.queryset)
