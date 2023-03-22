a30d635bc0b492a4de70d80ad666c86bde60216b
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2023-03-22 09:11:22 +0100

    changed timezone, created Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Book(models.Model):
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    pub_date = models.DateField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
