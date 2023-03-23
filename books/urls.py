15433fb7b8e8127ba8355d05da17e5e6f7b2bda8
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2023-03-23 11:36:41 +0100

    corrected urls, added post and delete methods

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,17 @@
 from rest_framework.routers import DefaultRouter
-from .views import BookList
+from .views import BookList, BookDetail
+from django.urls import path
 
-router = DefaultRouter()
+# router = DefaultRouter()
+#
+# router.register(r'books', BookList.as_view({'get': 'get'}), basename='book_list')
+# # router.register('books', BookList)
+#
 
-router.register(r'books', BookList, basename='book_list')
-# router.register('books', BookList)
+# router = DefaultRouter()
+# router.register(r'books', BookList, basename='books')
+#
+# urlpatterns = router.urls
 
-urlpatterns = router.urls
+urlpatterns = [path('books/', BookList.as_view(), name='books'),
+               path('books/<int:pk>/', BookDetail.as_view(), name='detail_book'), ]
--- books/views.py
+++ books/views.py
@@ -5,7 +5,52 @@ from .serializers import BooksSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets, status
+from django.shortcuts import get_object_or_404
 
+
+class BookList(APIView):
+    @action(detail=True, methods=['get'])
+    def get(self, request):
+        queryset = Book.objects.all()
+        a = BooksSerializer(queryset, many=True)
+        return Response(a.data)
+
+    def post(self, request):
+        a = BooksSerializer(data=request.data)
+        if a.is_valid():
+            a.save()
+            return Response(a.data)
+        return Response(a.errors)
+
+
+class BookDetail(APIView):
+    def get(self, request, pk):
+        queryset = Book.objects.get(pk=pk)
+        a = BooksSerializer(queryset)
+        return Response(a.data)
+
+    def delete(self, request, pk):
+        queryset = get_object_or_404(Book, pk=pk)
+        queryset.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def post(self, request, pk):
+        queryset = Book.objects.get(pk=pk)
+        print(queryset)
+        data = request.data
+
+        return Response()
+
+        # a = BooksSerializer(data=request.data)
+        # if a.is_valid():
+        #     a.save()
+        #     return Response(a.data)
+        # return Response(a.errors)
+
+    # @classmethod
+    # def get_extra_actions(cls):
+    #     return []
 
 # class BookList(APIView):
 #     @action(detail=True, methods=['get'])
@@ -14,10 +59,11 @@ from rest_framework.viewsets import ModelViewSet
 #         a = BooksSerializer(queryset, many=True)
 #         return Response(a.data)
 #
+#     #
 #     @classmethod
 #     def get_extra_actions(cls):
 #         return []
 
-class BookList(ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BooksSerializer
+# class BookList(ModelViewSet):
+#     queryset = Book.objects.all()
+#     serializer_class = BooksSerializer
